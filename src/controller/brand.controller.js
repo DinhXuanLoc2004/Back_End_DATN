0457576418568b685e0f7988fa7a118baddc16ef
ziz04 <zizdangeo0404@gmail.com>
2024-11-06 16:53:57 +0700

    #21 Tri/Create/BrandAndCategory

--- src/controller/brand.controller.js
+++ src/controller/brand.controller.js
@@ -3,12 +3,30 @@ const BrandService = require("../services/brand.service")
 
 class BrandController {
     static addBrand = async (req, res, next) => {
-        const { name_brand, image } = req.body
+        const { name_brand, image } = req.body;
         new CREATED({
             message: 'Create brand success!',
             metadata: await BrandService.addBrand({ name_brand, image })
-        }).send(res)
-    }
+        }).send(res);
+    };
+
+    static deleteBrand = async (req, res, next) => {
+        const { id } = req.query;
+        const result = await BrandService.deleteBrand(id);
+        res.json(result);
+    };
+
+    static getBrands = async (req, res, next) => {
+        const brands = await BrandService.getBrands();
+        res.json({ message: 'Brands fetched successfully', brands });
+    };
+
+    static updateBrand = async (req, res, next) => {
+        const { id } = req.query;
+        const updateData = req.body;
+        const result = await BrandService.updateBrand(id, updateData);
+        res.json(result);
+    };
 }
 
-module.exports = BrandController
+module.exports = BrandController;
--- src/services/brand.service.js
+++ src/services/brand.service.js
@@ -4,12 +4,43 @@ const { selectFilesData } = require("../utils")
 
 class BrandService {
     static addBrand = async ({ name_brand, image }) => {
-        const newBrand = await brandModel.create({ name_brand, image_brand: image })
-        if (!newBrand) throw new ConflictRequestError('Error create brand')
+        const newBrand = await brandModel.create({ name_brand, image_brand: image });
+        if (!newBrand) throw new ConflictRequestError('Error create brand');
         return {
             newBrand: selectFilesData({ fileds: ['name_brand', 'image_brand'], object: newBrand })
-        }
-    }
+        };
+    };
+
+    static deleteBrand = async (brandId) => {
+        const deletedBrand = await brandModel.findByIdAndUpdate(
+            brandId,
+            { is_deleted: true },
+            { new: true }
+        );
+        if (!deletedBrand) throw new Error('Brand not found or already deleted');
+        return {
+            message: 'Brand deleted successfully!',
+            brand: selectFilesData({ fileds: ['name_brand', 'image_brand', 'is_deleted'], object: deletedBrand })
+        };
+    };
+
+    static getBrands = async () => {
+        const brands = await brandModel.find({ is_deleted: false });
+        return brands.map((brand) => selectFilesData({ fileds: ['name_brand', 'image_brand'], object: brand }));
+    };
+
+    static updateBrand = async (brandId, updateData) => {
+        const updatedBrand = await brandModel.findByIdAndUpdate(
+            brandId,
+            updateData,
+            { new: true }
+        );
+        if (!updatedBrand) throw new Error('Brand not found');
+        return {
+            message: 'Brand updated successfully!',
+            brand: selectFilesData({ fileds: ['_id', 'name_brand', 'image_brand'], object: updatedBrand })
+        };
+    };
 }
 
-module.exports = BrandService
+module.exports = BrandService;
