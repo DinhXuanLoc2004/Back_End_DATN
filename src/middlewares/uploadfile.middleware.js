25a5f3febb7ad469cba293309bc18b274e062169
DinhXuanLoc2004 <dinhxuanlocct@gmail.com>
2024-12-10 01:08:48 +0700

    update middleware upload images

--- src/middlewares/uploadfile.middleware.js
+++ src/middlewares/uploadfile.middleware.js
@@ -36,34 +36,39 @@ const uploadSingleImageMiddleware = asyncHandler(async (req, res, next) => {
     next()
 })
 
+const uploadOldImages = asyncHandler(async(req, res, next) => {
+    console.log('images:: ', req.body.images);
+    next()
+})
+
 const uploadImageMiddleware = asyncHandler(async (req, res, next) => {
-    // if (!req.body.images || !Array.isArray(req.body.images)) {
-    //     return next()
-    // }
+    if (!req.files || !Array.isArray(req.files)) {
+        return next()
+    }
 
-    console.log(req.body.images);
-    // const images = req.body.images.reduce((files, file) => {
-    //     if (file.url && file.public_id && file.type) {
-    //         files.push(Promise.resolve(file))
-    //     } else {
-    //         if (pattern_media.test(file.originalname)) {
-    //             const resource_type = pattern.test(file.originalname) ? 'image' : 'video'
-    //             const asyncImage = new Promise((resolve, reject) => {
-    //                 cloudinary.uploader.upload_stream({ resource_type: resource_type },
-    //                     (err, uploadResult) => {
-    //                         if (err) reject(err)
-    //                         const { public_id, url } = uploadResult
-    //                         return resolve({ public_id, url, type: resource_type })
-    //                     }).end(file.buffer)
-    //             })
-    //             files.push(asyncImage)
-    //         }
-    //     }
-    //     return files
-    // }, [])
+    console.log(req.files);
+    const images = req.files.reduce((files, file) => {
+        if (file.url && file.public_id && file.type) {
+            files.push(Promise.resolve(file))
+        } else {
+            if (pattern_media.test(file.originalname)) {
+                const resource_type = pattern.test(file.originalname) ? 'image' : 'video'
+                const asyncImage = new Promise((resolve, reject) => {
+                    cloudinary.uploader.upload_stream({ resource_type: resource_type },
+                        (err, uploadResult) => {
+                            if (err) reject(err)
+                            const { public_id, url } = uploadResult
+                            return resolve({ public_id, url, type: resource_type })
+                        }).end(file.buffer)
+                })
+                files.push(asyncImage)
+            }
+        }
+        return files
+    }, [])
 
-    // const data = await Promise.all(images)
-    // req.body.images = data
+    const data = await Promise.all(images)
+    req.body.images = data
     return next()
 })
 
@@ -71,5 +76,6 @@ module.exports = {
     upload,
     uploadImageMiddleware,
     uploadSingleImageMiddleware,
-    deleteImageMiddleware
+    deleteImageMiddleware,
+    uploadOldImages
 }
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { upload, uploadImageMiddleware, } = require('../middlewares/uploadfile.middleware')
+const { upload, uploadImageMiddleware, uploadOldImages, } = require('../middlewares/uploadfile.middleware')
 const { asyncHandler } = require('../utils')
 const ProductController = require('../controller/product.controller')
 const router = express.Router()
 
-router.post('/add_product', upload.array(['images']), uploadImageMiddleware, asyncHandler(ProductController.addProduct))
+router.post('/add_product', upload.array(['images']), uploadOldImages, uploadImageMiddleware, asyncHandler(ProductController.addProduct))
 router.get('/get_products', asyncHandler(ProductController.getProducts))
 router.post('/get_all_products/', asyncHandler(ProductController.getAllProducts))
 router.get('/get_data_filter', asyncHandler(ProductController.getDataFilter))
