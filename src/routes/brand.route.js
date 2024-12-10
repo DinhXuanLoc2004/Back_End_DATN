3aae6056d343602c5a9b15e5bc7fa0be7912eda9
ziz04 <zizdangeo0404@gmail.com>
2024-12-10 09:22:23 +0700

    All

--- src/routes/brand.route.js
+++ src/routes/brand.route.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const { asyncHandler } = require('../utils')
-const BrandController = require('../controller/brand.controller')
-const { upload, uploadSingleImageMiddleware } = require('../middlewares/uploadfile.middleware')
-const router = express.Router()
+const express = require("express");
+const { asyncHandler } = require("../utils");
+const BrandController = require("../controller/brand.controller");
+const {
+  upload,
+  uploadSingleImageMiddleware,
+} = require("../middlewares/uploadfile.middleware");
+const router = express.Router();
 
-router.post('/add_brand', upload.single('image'), uploadSingleImageMiddleware,
-    asyncHandler(BrandController.addBrand))
-router.delete('/toggle_delete_brand', asyncHandler(BrandController.toggleDeleteBrand));
-router.get('/get_all_brands', asyncHandler(BrandController.getAllBrands));
-router.put('/update_brand', upload.single('image'), uploadSingleImageMiddleware,
-    asyncHandler(BrandController.updateBrand));
+router.post(
+  "/add_brand",
+  upload.single("image"),
+  uploadSingleImageMiddleware,
+  asyncHandler(BrandController.addBrand)
+);
+router.delete(
+  "/toggle_delete_brand",
+  asyncHandler(BrandController.toggleDeleteBrand)
+);
+router.get("/get_all_brands", asyncHandler(BrandController.getAllBrands));
+router.put(
+  "/update_brand",
+  upload.single("image"),
+  uploadSingleImageMiddleware,
+  asyncHandler(BrandController.updateBrand)
+);
 
-module.exports = router
+module.exports = router;
